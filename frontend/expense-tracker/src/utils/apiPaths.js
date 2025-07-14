84208699f244cbbe5975d81496eb4b89c6a6cac8
Harshil Ujjaval Desai <hdesai098@gmail.com>
2025-07-14 11:02:21 +0530

    Update apiPaths.js

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -3,7 +3,7 @@ export const BASE_URL = import.meta.env.VITE_API_URL
 // utils/apiPaths.js
 export const API_PATHS = {
     AUTH: {
-        LOGIN: "/api/v1/auth/login",
+        LOGIN: "/api/v1/auth/",
         REGISTER: "/api/v1/auth/register",
         GET_USER_INFO: "/api/v1/auth/getUser",
     },
