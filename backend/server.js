b7f9e9f3a4ab07fe6837e81cd8d45b8e068444e1
Harshil Ujjaval Desai <hdesai098@gmail.com>
2025-07-14 10:26:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 //Middleware to handle CORS
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'https://expense-tracker-somb.onrender.com',
+    origin: 'https://expense-tracker-somb.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
